41b28101508b5aa13264f4af9481f1fa8868cb28
Victor <Vctorsenin@yandex.ru>
2024-11-08 11:48:53 +0300

    module-2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries){
+    if(isFinite(salaries[key]) && salaries[key]){
+      console.log(salaries[key]);
+      sum+=salaries[key];
+    } 
+  }
+  return sum
 }
