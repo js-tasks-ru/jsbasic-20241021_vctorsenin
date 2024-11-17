d9d8b6bd81d7bc9cb2f254cf575931c616058789
Victor <Vctorsenin@yandex.ru>
2024-11-17 13:13:45 +0300

    4 module

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const list = document.createElement("ul");
+  friends.forEach((elem, index) => {
+    let str = document.createElement("li");
+    str.textContent = `${elem.firstName} ${elem.lastName}`;
+    list.append(str);
+  });
+  
+  return list;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,20 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 0; i < table.rows.length; i++){
+    if(table.rows[i].cells[3].dataset.available== 'true' ){
+      table.rows[i].classList.add('available')
+    }else{
+      table.rows[i].classList.add('unavailable')
+    }
+    if(!table.rows[i].cells[3].dataset.available ){
+      table.rows[i].setAttribute('hidden',"")
+    }
+    if(table.rows[i].cells[2].innerText == 'm'){
+      table.rows[i].classList.add('male')
+    }else{
+      table.rows[i].classList.add('female')
+    }
+    if (parseInt(table.rows[i].cells[1].innerText) < 18){
+      table.rows[i].style.textDecoration = 'line-through';
+    }
+  }
 }
